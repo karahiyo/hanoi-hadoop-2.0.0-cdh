88e63474d3bb6c3ed7922d2560e909d32bd574c9
karahiyo <a.ryuklnm@gmail.com>
2013-11-06 22:52:17 +0900

    remove old monitor scripts.

--- src/monitor/aj/hadoop/monitor/mapreduce/MapperMonitor.java
+++ src/monitor/aj/hadoop/monitor/mapreduce/MapperMonitor.java
@@ -19,7 +19,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 
-import com.github.karahiyo.hanoi_picker;
+import com.github.karahiyo.hanoi_picker.*;
 
 @Aspect
 public class MapperMonitor {
